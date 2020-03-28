421ca69073b6842e35d44ff54ffffcfd73c22b12
Thomas Bos <thymer.bos217@gmail.com>
2020-03-28 15:42:00 +0100

    Removed accidental breaklines.

--- sklearn/exceptions.py
+++ sklearn/exceptions.py
@@ -192,8 +192,6 @@ class SkipTestWarning(UserWarning):
 class UndefinedMetricWarning(UserWarning):
     """Warning used when the metric is invalid
 
-    
-
     .. versionchanged:: 0.18
        Moved from sklearn.base.
     """
